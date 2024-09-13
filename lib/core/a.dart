f70654f5e195e4c645ff008fa87a5159e10390e3
Virat Patel <118610161+ViRatRibadiya1204@users.noreply.github.com>
2024-09-13 22:52:37 +0530

    feat: add core folder

---
+++ lib/core/a.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _mcState extends StatefulWidget {
+  const _mcState({super.key});
+
+  @override
+  State<_mcState> createState() => __mcStateState();
+}
+
+class __mcStateState extends State<_mcState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
